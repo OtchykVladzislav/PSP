8a06ebfd493ffd365c319abb4d619a4f666904f6
Otchyk Vladzislav <vladotchik-265@mail.ru>
2022-12-02 21:11:40 +0300

    Lab6

---
+++ Lab6/Lab5/MyMath.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    public static class MyMath
+    {
+        public static double GetSin(double angle)
+        {
+            var rad = angle / 57.2958;
+            double sin = 0;
+            int pow = 1;
+
+            for (int i = 0; i < 10; i++)
+            {
+                int k = 2 * i + 1;
+                sin += Math.Pow((-1), i) * (Math.Pow(rad, k) / Factorial(k));
+            }
+
+            return Math.Round(sin, 5);
+        }
+
+        public static int Factorial(int value)
+        {
+            int res = 1;
+
+            for (int i = 1; i <= value; i++)
+            {
+                res *= i;
+            }
+
+            return res;
+        }
+    }
+}
---
+++ Lab6/Lab5/RequestHandler.cs
@@ -0,0 +1,112 @@
+﻿using Lab5.Pages;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Lab5
+{
+    public class RequestHandler
+    {
+        private TcpClient _client;
+        private X509Certificate2 _serverSertificate;
+
+        public RequestHandler(TcpClient client, X509Certificate2 serverSertificate)
+        {
+            _client = client;
+            _serverSertificate = serverSertificate;
+        }
+
+        public void ProcessRequest(object state)
+        {
+            var sslStream = new SslStream(_client.GetStream(), false);
+
+            try
+            {
+                sslStream.AuthenticateAsServer(_serverSertificate, false, SslProtocols.Tls12, false);
+            }
+            catch
+            {
+            }
+
+            var request = ReadMessage(sslStream);
+
+            Console.WriteLine(request);
+
+            Page responsePage;
+
+            try
+            {
+                if (request.StartsWith("POST"))
+                {
+                    Regex regex = new Regex("\r\n\r\n");
+                    string[] requestItems = regex.Split(request, 2);
+
+                    Console.WriteLine(requestItems[1]);
+
+                    try
+                    {
+                        var bodyItem = requestItems[1].Split("=").Last();
+                        _ = double.TryParse(bodyItem, out double res);
+                        var sin = MyMath.GetSin(res);
+
+                        responsePage = new ResultPage(bodyItem, sin.ToString());
+                    }
+                    catch
+                    {
+                        responsePage = new BadPage();
+                    }
+                }
+                else
+                {
+                    responsePage = new IndexPage();
+                }
+            }
+            catch
+            {
+                responsePage = new BadPage();
+            }
+
+            try
+            {
+                byte[] response = Encoding.UTF8.GetBytes(responsePage.View());
+                sslStream.Write(response, 0, response.Length);
+                sslStream.Flush();
+            }
+            catch
+            {
+            }
+
+            sslStream.Close();
+            _client.Close();
+        }
+
+        private string ReadMessage(SslStream clientStream)
+        {
+            StringBuilder messageData = new StringBuilder();
+            try
+            {
+                byte[] buffer = new byte[2048];
+                int bytes = -1;
+
+                do
+                {
+                    bytes = clientStream.Read(buffer, 0, buffer.Length);
+                    Decoder decoder = Encoding.UTF8.GetDecoder();
+                    char[] chars = new char[decoder.GetCharCount(buffer, 0, bytes)];
+                    decoder.GetChars(buffer, 0, bytes, chars, 0);
+                    messageData.Append(chars);
+                }
+                while (bytes != 0);
+            }
+            catch (Exception)
+            {
+
+            }
+
+            return messageData.ToString();
+        }
+    }
+}
